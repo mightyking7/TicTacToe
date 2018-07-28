7e88221aa13876f6d6542db746b4e0ef6eddd9b5
Isaac Buitrago <mightyking7@gmail.com>
2018-07-28 16:30:09 -0500

    Implemented jump back functionality to view history of all past moves in the game

--- src/index.js
+++ src/index.js
@@ -60,7 +60,7 @@ class Game extends React.Component {
 
     handleClick(i)
     {
-        const history =  this.state.history;
+        const history =  this.state.history.slice(0, this.state.stepNumber + 1);
 
         const current = history[history.length - 1];
 
@@ -78,7 +78,19 @@ class Game extends React.Component {
                 squares: squares,
             }]),
 
+            stepNumber: history.length,
+
             xIsNext: !this.state.xIsNext,
+        });
+    }
+
+    jumpTo(step)
+    {
+        this.setState({
+
+            stepNumber: step,
+
+            xIsNext: (step % 2) === 0,
         });
     }
 
@@ -86,7 +98,7 @@ class Game extends React.Component {
 
         const history  = this.state.history;
 
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
 
         const winner = calculateWinner(current.squares);
 
@@ -127,7 +139,7 @@ class Game extends React.Component {
             </div>
             <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
             </div>
             </div>
     );

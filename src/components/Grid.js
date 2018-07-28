e4431ba54caae54af66358214411bcf1891e9262
Isaac Buitrago <mightyking7@gmail.com>
2018-07-28 15:25:46 -0500

    Created Game, Board, and Square components and handlers to handle clicks on the square

---
+++ src/components/Grid.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Grid extends React.Component
+{
+    render(){
+
+        <div class ="Member-Info-Grid">
+            <p> I am here !</p>
+        </div>
+
+    }
+
+
+}
